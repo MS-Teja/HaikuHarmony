b2786da641202e705ee2507d194efa4b347dcf9f
MS-Teja <sivatejamutyala@gmail.com>
2024-10-12 21:00:28 +0530

    completed design of navbar

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,22 @@
         <router-link to="/" class="nav-logo">Haiku Harmony</router-link>
       </div>
       <div class="navbar-center" :class="{ 'search-active': isSearchActive }">
-        <form @submit.prevent="performSearch" class="search-form">
+        <form @submit.prevent="performSearch" class="search-form group">
+          <svg viewBox="0 0 24 24" aria-hidden="true" class="form-search-icon">
+            <g>
+              <path
+                d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
+              ></path>
+            </g>
+          </svg>
           <input
             v-model="searchQuery"
-            type="text"
-            placeholder="Search tags..."
             class="search-input"
+            type="search"
+            placeholder="Search Haikus..."
+            name="searchbar"
             @keydown.esc="closeSearch"
-          >
-          <button type="submit" class="search-button">Search</button>
+          />
         </form>
       </div>
       <div class="navbar-right">
@@ -63,7 +70,7 @@
 </template>
 
 <script>
-import { ref, onMounted, onUnmounted } from 'vue';
+import { ref,onMounted, onUnmounted } from 'vue';
 import { auth } from './services/firebase';
 import { signOut } from 'firebase/auth';
 import { useRouter } from 'vue-router';
@@ -261,7 +268,7 @@ export default {
 .search-icon {
   display: none;
   cursor: pointer;
-  margin-right: 1rem;
+  margin-top: 5px;
 }
 
 .burger {
@@ -272,6 +279,21 @@ export default {
   cursor: pointer;
   display: none;
   z-index: 1001;
+}
+
+.burger::after {
+  content: '';
+  position: absolute;
+  top: 100%; /* Position it right at the bottom of the burger */
+  left: 0;
+  right: 0;
+  height: 5px; /* Extend 5px below */
+  cursor: pointer;
+}
+
+.burger-wrapper {
+  position: relative;
+  display: inline-block;
 }
 
 .burger input {
@@ -350,7 +372,118 @@ export default {
   -webkit-backdrop-filter: blur(5px);
 }
 
+.group {
+  display: flex;
+  line-height: 28px;
+  align-items: center;
+  position: relative;
+  width: 300px;
+  margin-left: 200px;
+}
+
+.search-input {
+  font-family: "cormorant", "Montserrat", sans-serif;
+  width: 100%;
+  height: 35px;
+  padding-left: 2.5rem;
+  padding-top:1.5px;
+  padding-bottom:1px;
+  box-shadow: 0 0 0 1.5px #2b2c37, 0 0 25px -17px #000;
+  border: 0;
+  border-radius: 12px;
+  background-color: white;
+  outline: none;
+  color: #bdbecb;
+  transition: all 0.25s cubic-bezier(0.19, 1, 0.22, 1);
+  cursor: text;
+  z-index: 0;
+}
+
+.search-input::placeholder {
+  color: #000000;
+}
+
+.search-input:hover {
+  box-shadow: 0 0 0 2.5px #2f303d, 0px 0px 25px -15px #000;
+}
+
+.search-input:active {
+  transform: scale(0.95);
+}
+
+.search-input:focus {
+  box-shadow: 0 0 0 2.5px #2f303d;
+}
+
+.form-search-icon {
+  position: absolute;
+  left: 1rem;
+  fill: #000000;
+  width: 1rem;
+  height: 1rem;
+  pointer-events: none;
+  z-index: 1;
+}
+
+@media (max-width: 1259px){
+  .group {
+    margin-left: 150;
+  }
+}
+
+@media (max-width: 1145px) {
+  .group {
+    margin-left: 100px;
+  }
+}
+
+@media (max-width: 1029px) {
+  .group {
+    margin-left: 50px;
+  }
+}
+
+@media (max-width: 981px) {
+  .group {
+    margin-left: 50px;
+  }
+
+  .search-input {
+    width: 80%;
+  }
+
+  .navbar-center{
+    width: 300px;
+  }
+}
+
+@media (max-width: 931px) {
+  .navbar-center{
+    width: 250px;
+  }
+}
+
+@media (max-width: 882px) {
+  .navbar-center{
+    width: 200px;
+  }
+}
+
 @media (max-width: 846px) {
+  .group {
+    margin-left: 0px;
+  }
+
+  .search-input {
+    width: 100%;
+  }
+
+  .navbar-center{
+    width: 80%;
+    align-self: center;
+    transform: translateX(8%)
+  }
+
   .navbar {
     padding: 1rem;
   }
@@ -432,6 +565,16 @@ export default {
 
   .search-icon {
     display: block;
+    padding-bottom: 2px;
+  }
+
+  .account-profile {
+    margin: 0 0.5rem;
+  }
+}
+@media (max-width: 400px) {
+    .account-profile {
+      margin: 0;
   }
 }
 </style>
